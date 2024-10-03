b8503f6d89edf2a05ad28d998e5c78d65739a323
youssef <youssefkardous1@gmail.com>
2024-10-03 06:27:10 +0100

    Upgrade Agent

--- src/agents.py
+++ src/agents.py
@@ -1,29 +1,31 @@
 from langchain_groq import ChatGroq 
-from langchain.sql_database import SQLDatabase
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain_experimental.tools.python.tool import PythonREPLTool
-from langchain.agents import initialize_agent
-from langchain.agents import AgentType
+from langchain.agents.agent_toolkits import FileManagementToolkit
+from langchain.agents import create_structured_chat_agent
+from langchain.agents import AgentExecutor
 from src import template
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import os
 def create_SQL_agent(db, groq_api):
     llm=ChatGroq(groq_api_key=groq_api,model_name="Llama3-8b-8192",streaming=True)
-    tools = []
+    working_directory  = os.getcwd()
+    tools = FileManagementToolkit(
+        root_dir=str(working_directory),
+        selected_tools=["read_file", "write_file", "list_directory"],).get_tools()
     tools.append(PythonREPLTool())
     tools.extend(SQLDatabaseToolkit(db=db, llm=llm).get_tools())
-
-    agent = initialize_agent(
-        tools=tools, 
-        llm=llm, 
-        agent= AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
-        verbose=True,
-        handle_parsing_errors=True,
-        agent_kwargs={
-            'prefix':template.PREFIX,
-            'format_instructions': template.FORMAT_INSTRUCTIONS,
-        }    
+        
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", template.system),
+            MessagesPlaceholder("chat_history", optional=True),
+            ("human", template.human),
+        ]
     )
-    return agent
+    agent = create_structured_chat_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+    return agent_executor
 
 def run_agent(agent, query, callbacks):    
-    return agent.invoke(query, callbacks=callbacks)
+    return agent.invoke({"input":{query}}, callbacks=callbacks)
--- src/template.py
+++ src/template.py
@@ -1,29 +1,47 @@
-PREFIX = """ 
-You are an advanced AI agent specialized in SQL database interactions, data analysis, and visualization. Your primary functions are:
+system = '''You are an advanced AI agent specializing in SQL database interactions, data analysis, and visualization. You have access to the following tools:
+{tools}
+Use a JSON blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).
+Valid "action" values: "Final Answer" or {tool_names}
+Provide only ONE action per $JSON_BLOB, as shown:
+```
+{{
+  "action": $TOOL_NAME,
+  "action_input": $INPUT
+}}
+```
+Follow this format:
+Question: input question to answer
+Thought: consider previous and subsequent steps
+Action:
+```
+$JSON_BLOB
+```
+Observation: action result
+... (repeat Thought/Action/Observation N times)
+Thought: I know what to respond
+Action:
+```
+{{
+  "action": "Final Answer",
+  "action_input": "Final response to human"
+}}s
+Here’s how you should approach each task:
 
-1. Execute SQL queries on the provided database
-2. Analyze the query results
-3. Generate and save! relevant matplotlib visualizations in the current working directory
+1. **SQL Query**: Execute the SQL query to retrieve data. 
+2. **Analysis**: Use the Python_REPL tool to analyze the query results, create relevant pandas DataFrames if necessary.
+3. **Visualization**: Use the Python_REPL tool to generate matplotlib visualizations from the query results and save the plot as a PNG file.
+4. **Error Handling**: If errors occur (empty results, invalid data, plot generation issues), handle them gracefully and retry if necessary.
 
-Always ensure your response includes:
-- The direct answer to the user's question
-- An explanation of your approach
-- The filename and description of the saved matplotlib plot
 
-Remember to handle potential errors gracefully, such as empty query results or issues with plot generation.
-"""
+Always ensure the Final response to human is well seperated, organized and includes:
+1- The direct answer to the user's question.
+2- The SQL query  that was used.
+3- Python code   that was used to create and save the plot.
+4- The filename and description of the saved matplotlib plot.
 
-FORMAT_INSTRUCTIONS = """ Use the following format:
-Question: the input question you must answer
-Thought: you should always think about what to do
-Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
-Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I now know the final answer
-Final Answer: the final answer to the original input question"""
 
-SUFFIX = """Begin!
-
-Question: {input}
-Thought:{agent_scratchpad}"""
+Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary. Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation'''
+human = '''
+{input}
+{agent_scratchpad}
+(reminder to respond in a JSON blob no matter what)'''
